07985e4438155ef9ffcf04aad6cdc6bc256972ad
mahamadou-nouridine <mahamadounouridinem@gmail.com>
2023-12-09 12:11:20 +0100

    Add cors

--- server/app.js
+++ server/app.js
@@ -2,12 +2,15 @@
 const express = require('express');
 const http = require('http');
 const {Server} = require('socket.io');
+const cors = require("cors")
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {cors:{
   origin: true
 }});
+
+app.use(cors())
 
 app.use(express.json());
 
